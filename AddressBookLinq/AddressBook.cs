c237bc00ededd755d67046377fbc8ba55ddc7a3d
aayusharya1 <aayusharya07@gmail.com>
2020-11-16 18:40:04 +0530

    [Aayush] Add. Code for adding contacts to DataTable

--- AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBook.cs
@@ -22,5 +22,18 @@ namespace AddressBookLinq
             dataTable.Columns.Add("PhoneNumber", typeof(string));
             dataTable.Columns.Add("EmailID", typeof(string));
         }
+        /// <summary>
+        /// Insert Contacts in a the addressBook
+        /// </summary>
+        public void InsertContacts()
+        {
+            
+            dataTable.Rows.Add("Aayush", "Arya", "Street xyz", "New Delhi", "Delhi", "110000", "9999999999", "aayush@gmail.com");
+            dataTable.Rows.Add("Aayus", "Arya", "Street xyzww", "New Delhi", "Delhi", "110001", "9999999998", "aayus@gmail.com");
+            dataTable.Rows.Add("Aayu", "Arya", "Street xyzqq", "Navi Mumbai", "Mumbai", "210000", "9999999988", "aayu@gmail.com");
+            dataTable.Rows.Add("Aayush", "Ary", "Street abcxyz", "Pilani", "Rajasthan", "310000", "8999999999", "aayush1@gmail.com");
+            dataTable.Rows.Add("Aay", "Arya", "Street xyzop", "New Delhi", "Delhi", "110004", "8899999999", "aay@gmail.com");
+        }
+
     }
 }
